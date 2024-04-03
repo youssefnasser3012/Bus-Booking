4567d509bc39ffe4c58b65a5441313e49c443d0e
youssefnasser3012 <Youssef_20211113@fci.helwan.edu.eg>
2024-04-03 06:05:09 +0200

    Update navbar.js

--- my-react-app/src/component/Account/navbar.js
+++ my-react-app/src/component/Account/navbar.js
@@ -1,7 +1,4 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
+
 import {Navbar,Nav,Container} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './navbar.css'
@@ -17,14 +14,14 @@ return (
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse  className="collapse_color" id="responsive-navbar-nav">
       
-      <Nav className="nav_links ms-auto pl-3">
-      <Link to="/" style={{textDecoration:'none'}}> <Nav.Item href="/home">Home</Nav.Item></Link>
-      <Link to="/tickets"  style={{textDecoration:'none'}}><Nav.Item href="/tickets">Tickets</Nav.Item></Link>
-      <Link to="/traveldistination" style={{textDecoration:'none'}}><Nav.Item href="/traveldistination">Travel Distination</Nav.Item></Link>
-      <Link to="/history" style={{textDecoration:'none'}}><Nav.Item href="/history">History</Nav.Item></Link>
-      <Link to="/busstation" style={{textDecoration:'none'}}><Nav.Item href="/busstation">Bus Station</Nav.Item></Link>
-      <Link to="/ourclasses" style={{textDecoration:'none'}}><Nav.Item href="/ourclasses">Our Classes</Nav.Item></Link>
+         <Nav className="nav_links ms-auto pl-3">
       
+      <Link to="/" style={{textDecoration:'none'}}> <Nav.Item href="/home">Home</Nav.Item></Link>
+      <Link to="/updatetickets"  style={{textDecoration:'none'}}><Nav.Item href="/updatetickets">Update Tickets</Nav.Item></Link> 
+    
+       <Link to="/request" style={{textDecoration:'none'}}><Nav.Item href="/request">Requests</Nav.Item></Link>
+  
+   <Link to="/manageusers" style={{textDecoration:'none'}}><Nav.Item href="/manageusers"> Manage Users</Nav.Item></Link>
       </Nav>
     
     
